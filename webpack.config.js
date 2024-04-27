263a6b3163a73faf24ed8cdeb0ca58b289b0212e
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-04-27 11:28:52 -0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+/** @type {import("@types/webpack").Configuration} */
+module.exports = {
+   mode: NODE_ENV === 'development' ? NODE_ENV : 'production',
+   entry: './src/index.tsx',
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true,
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            use: ['babel-loader', 'ts-loader'],
+         },
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader', 'postcss-loader']
+         }
+      ]
+   },
+   resolve: {
+      extensions: [
+         '.js',
+         '.jsx',
+         '.ts',
+         '.tsx',
+      ]
+   },
+   plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+}
